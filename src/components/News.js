9434cd9a6cd0e3883fde2114a00949710557af74
Mohammed Faizan Mohiuddin <faizanmohiuddin482@gmail.com>
2021-09-22 15:39:41 +0530

    added date picker

--- src/components/News.js
+++ src/components/News.js
@@ -7,23 +7,42 @@ import {
   SafeAreaView,
   ActivityIndicator,
 } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import NetInfo from '@react-native-community/netinfo';
+import DatePicker from 'react-native-datepicker';
+import moment from 'moment';
 import axios from "react-native-axios";
 import { API_KEY } from "../config/env";
 import Article from "./Article";
 
-const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${API_KEY}&pageSize=5&page=`;
-
 const News = () => {
   const [articles, setArticles] = useState([]);
-  const [data, setData] = useState();
+  const [date, setDate] = useState(moment().format("YYYY/MM/DD"));
+  const [toDate, setToDate] = useState(moment().format("YYYY/MM/DD"));
+  const [oflineData, setOflineData] = useState();
   const [loading, setLoading] = useState(false);
   const [dataSource, setDataSource] = useState([]);
   const [offset, setOffset] = useState(1);
   const [isListEnd, setIsListEnd] = useState(false);
+  const [netInfo, setNetInfo] = useState(false);
+
+  const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${API_KEY}&pageSize=5&from=${date}&to=${toDate}&page=`;
 
   useEffect(() => {
     getData();
-  }, []);
+	getAsyncData()
+  }, [date,dataSource]);
+
+  NetInfo.fetch().then(state => {
+	  if(state.isConnected){
+	  setNetInfo(true);
+	  }else{
+		  setNetInfo(false);
+	  }
+	// console.log('Connection type', state.type);
+	// console.log('Is connected?', state.isConnected);
+  });
+  
 
   const getData = () => {
     console.log(offset);
@@ -32,15 +51,13 @@ const News = () => {
       setLoading(true);
       // Service to get the data from the server to render
       axios
-        .get(
-          `https://newsapi.org/v2/top-headlines?country=in&apiKey=${API_KEY}&pageSize=5&page=` +
-            offset
-        )
+        .get(url + offset)
         .then((response) => {
           // Successful response from the API Call
           console.log(response.data.articles.length, "jsonnnnnnnnnnnn");
           if (response.data.articles.length > 0) {
             setDataSource([...dataSource, ...response.data.articles]);
+			storeData([...dataSource, ...response.data.articles])
             setOffset(offset + 1);
             setLoading(false);
           } else {
@@ -53,6 +70,31 @@ const News = () => {
         });
     }
   };
+
+  const storeData = async (value) => {
+	try {
+	  const jsonValue = JSON.stringify(value)
+	  await AsyncStorage.setItem('dataStore', jsonValue)
+	} catch (e) {
+	  // saving error
+	}
+  }
+
+ 
+const getAsyncData = async () => {
+	try {
+	  const jsonValue = await AsyncStorage.getItem('dataStore')
+	
+	if (jsonValue !== null) {
+		// We have data!!
+		setOflineData(JSON.parse(jsonValue));
+	  }
+	
+	} catch(e) {
+	  // error reading value
+	}
+  }
+
 
   const renderFooter = () => {
     return (
@@ -67,8 +109,58 @@ const News = () => {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
+		<View style={{flexDirection: 'row',justifyContent: 'space-between',padding:10}}>
+	 <DatePicker
+          style={styles.datePickerStyle}
+          date={date} // Initial date from state
+          mode="date" // The enum of date, datetime and time
+          placeholder="select date"
+          format="YYYY-MM-DD"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateIcon: {
+              //display: 'none',
+              position: 'absolute',
+              left: 0,
+              top: 4,
+              marginLeft: 0,
+            },
+            dateInput: {
+              marginLeft: 36,
+            },
+          }}
+          onDateChange={(date) => {
+            setDate(date);
+          }}
+        /> 
+		<DatePicker
+		style={styles.datePickerStyle}
+		date={date} // Initial date from state
+		mode="date" // The enum of date, datetime and time
+		placeholder="select date"
+		format="YYYY-MM-DD"
+		confirmBtnText="Confirm"
+		cancelBtnText="Cancel"
+		customStyles={{
+		  dateIcon: {
+			//display: 'none',
+			position: 'absolute',
+			left: 0,
+			top: 4,
+			marginLeft: 0,
+		  },
+		  dateInput: {
+			marginLeft: 36,
+		  },
+		}}
+		onDateChange={(date) => {
+		  setToDate(date);
+		}}
+	  />
+	  </View>
       <FlatList
-        data={dataSource}
+        data={!netInfo && oflineData != null ? oflineData : dataSource}
         renderItem={({ item }) => (
           <Article
             title={item.title}
